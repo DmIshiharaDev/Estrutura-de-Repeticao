23bea141332e65bf79062f601acde84ef474d540
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-18 13:33:49 -0300

    Adicionar arquivos de projeto.

---
+++ for/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int cont = 0; cont <= 100; cont++) 
+        {
+            Console.WriteLine(cont);
+        }
+
+        Console.ReadKey();
+    }
+}
